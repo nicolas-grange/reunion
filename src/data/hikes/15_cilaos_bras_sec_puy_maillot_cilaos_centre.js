a54dd698343c402fab3210eb869df056c97a94be
ngrange <ngrange@talend.com>
2023-11-20 12:03:25 +0400

    add hikes

--- src/data/hikes/15_cilaos_bras_sec_puy_maillot_cilaos_centre.js
+++ src/data/hikes/15_cilaos_bras_sec_puy_maillot_cilaos_centre.js
@@ -1,5 +1,5 @@
 export const h15_cilaos_bras_sec_puy_maillot_cilaos_centre = {
-  "id": "hike-13",
+  "id": "hike-15",
   "title": "Bras Sec à Cilaos",
   "difficulty": "facile",
   "distance" : "6.20km",
